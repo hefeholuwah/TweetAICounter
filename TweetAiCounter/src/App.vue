7e2a6ac5aee1f33bffa07d19eadfe7a7ea91be36
ifeoluwa Adefioye <ifeoluwa@Seuns-MacBook-Air.local>
2024-08-22 21:22:50 +0100

    simple counter page

---
+++ TweetAiCounter/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1>Autobots Count: {{ autobotsCount }}</h1>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      autobotsCount: 0,
+    };
+  },
+  created() {
+    this.fetchAutobotsCount();
+    setInterval(this.fetchAutobotsCount, 10000); // Fetch every 10 seconds
+  },
+  methods: {
+    async fetchAutobotsCount() {
+      const response = await axios.get(
+        "http://localhost:3000/api/autobots/count"
+      );
+      this.autobotsCount = response.data.count;
+    },
+  },
+};
+</script>
